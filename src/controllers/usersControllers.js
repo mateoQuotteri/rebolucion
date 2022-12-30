de74db073b77b40970e12e3ee7712424c20410f3
Mateo Quotteri <quotterimateo@gmail.com>
2022-12-30 12:59:00 -0300

    trying to solve controllers problems

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,5 +1,17 @@
 module.exports = {
     register : (req,res)=>{
         res.render("register")
-    }
+    },
+    createNewUser: (req,res)=>{
+        const newUser = req.body;
+        console.log(newUser);
+        res.render("login")
+    },
+    showLogin : (req,res)=>{
+        res.render("login")
+    },
+    login: (req,res)=>{
+        /*CODIGO DEL LOGIN*/ 
+    },
+
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,8 @@ const userController = require("../controllers/usersControllers")
 /*const productController = require("../controllers/productsControllers")*/
 
 router.get("/register", userController.register)
+router.post("/register", userController.createNewUser)
 
-
+router.get("/login", userController.showLogin)
+router.post("/login", userController.login)
 module.exports = router
