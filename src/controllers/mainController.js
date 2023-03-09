fc5b14624926a83a4be54c3eea0f0c72edeeec37
Mateo Quotteri <quotterimateo@gmail.com>
2023-03-09 20:58:50 -0300

    mainController changes

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,10 @@
 module.exports = {
     index : (req,res)=>{
-        res.render("home")
+        const thingsToLearn = ["Web3",
+         "Blockchain", 
+         "Contratos inteligentes", 
+        "Finanzas Descentralizadas", "Bitcoin" 
+        , "Ethereum"]
+        res.render("home", { thingsToLearn})
     }
 }
