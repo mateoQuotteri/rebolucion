15ad9e23dc91034da78b301f9b03b40a79b08725
Mateo Quotteri <quotterimateo@gmail.com>
2023-03-16 19:26:05 -0300

    a lot of new features on home views

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,31 @@
 module.exports = {
     index : (req,res)=>{
-        const thingsToLearn = ["Web3",
-         "Blockchain", 
-         "Contratos inteligentes", 
-        "Finanzas Descentralizadas", "Bitcoin" 
-        , "Ethereum"]
+
+        const thingsToLearn= [{
+            name: "Web3",
+            image: "web3Card.png"
+           },
+           {
+            name: "Blockchain",
+            image: "blockchainCard.png"
+           },
+           {
+            name: "Contartos inteligentes",
+            image: "contratoCard.png"
+           },
+           {
+            name: "Finanzas Descentralizadas",
+            image: "defiCard.png"
+           },
+           {
+            name: "Bitcoin",
+            image: "bitcoinCard.png"
+           },
+           {
+            name: "Ethereum",
+            image: "ethereumCard.png"
+           },
+        ]
         res.render("home", { thingsToLearn})
     }
 }
