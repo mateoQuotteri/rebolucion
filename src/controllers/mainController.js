c3cfed28260cb557ee0871c6be589ce0ec136513
Mateo Quotteri <quotterimateo@gmail.com>
2023-03-17 15:28:19 -0300

    styles of card in proggess and ok (before all crash)

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,27 +3,33 @@ module.exports = {
 
         const thingsToLearn= [{
             name: "Web3",
-            image: "web3Card.png"
+            image: "web3Card.png",
+            icon : "fa-regular fa-globe",
            },
            {
             name: "Blockchain",
-            image: "blockchainCard.png"
+            image: "blockchainCard.png",
+            icon : "fa-solid fa-cubes",
            },
            {
             name: "Contartos inteligentes",
-            image: "contratoCard.png"
+            image: "contratoCard.png",
+            icon : "fa-solid fa-file-contract"
            },
            {
             name: "Finanzas Descentralizadas",
-            image: "defiCard.png"
+            image: "defiCard.png",
+            icon : "fa-thin fa-chart-simple",
            },
            {
             name: "Bitcoin",
-            image: "bitcoinCard.png"
+            image: "bitcoinCard.png",
+            icon : "fa-brands fa-bitcoin",
            },
            {
             name: "Ethereum",
-            image: "ethereumCard.png"
+            image: "ethereumCard.png",
+            icon : "fa-brands fa-ethereum",
            },
         ]
         res.render("home", { thingsToLearn})
