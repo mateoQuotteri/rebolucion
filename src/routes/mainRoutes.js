099bf6549c26c2f035f0ec773ccc5b0d54fa2d4b
Mateo Quotteri <quotterimateo@gmail.com>
2023-03-20 08:35:57 -0300

    adding middlewares to main routes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,11 +2,11 @@ const express = require("express")
 const router = express.Router()
 const mainController = require("../controllers/mainController")
 /*const productController = require("../controllers/productsControllers")*/
-
+const authMiddleware = require("../middlewares/authMiddleware")
 router.get("/", mainController.index)
 /*router.get("/adminpanel", mainController.admin)*/
 
-router.get("/aviso", mainController.aviso)
+router.get("/aviso", authMiddleware ,mainController.aviso)
 
-router.get("/contact-us", mainController.contact)
+router.get("/contact-us", authMiddleware,mainController.contact)
 module.exports = router
