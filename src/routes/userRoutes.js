5efc369d0a7352f0ab6b9c13406a044849c07197
Mateo Quotteri <quotterimateo@gmail.com>
2023-03-22 19:44:13 -0300

    now if the user is tryng to register with an email which is in my dbthe error shows on register views

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,9 +6,10 @@ const loginValidation = require("../validations/loginValidation");
 const changePassValidation = require("../validations/changePassValidation")
 const guestMiddleware = require("../middlewares/guestMiddleware")
 const authMiddleware = require("../middlewares/authMiddleware")
+const reEmailMiddleware = require("../middlewares/reEmailMiddleware")
 
 router.get("/register", guestMiddleware ,userController.register)
-router.post("/register", registerValidation, userController.createNewUser)
+router.post("/register", registerValidation, reEmailMiddleware, userController.createNewUser)
 
 router.get("/login",  guestMiddleware ,userController.showLogin)
 router.post("/login",  loginValidation ,userController.login)
