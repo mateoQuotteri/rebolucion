b463dae86421ea2e9b81a12f236c08e00c84e007
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-10 22:06:44 -0300

    showPass button onclick eyeslashed appears

--- public/js/showPassOnLogin.js
+++ public/js/showPassOnLogin.js
@@ -9,8 +9,15 @@ window.onload = function () {
     const button = document.getElementById("showPass")
     
     button.addEventListener("click", function () {
-        console.log("hola");
-        console.log("Este es el boton "+button);
+        console.log(button.className);
+        if(button.className.includes("showPass")) {
+      button.classList.add("eye-x");
+      button.classList.remove("showPass")
+    }else {
+        button.classList.remove("eye-x");
+        button.classList.add("showPass")
+    }
+      
         if (password.type == "password" ) {
             password.type = "text";
            
--- public/js/showPassword.js
+++ public/js/showPassword.js
@@ -1,10 +1,16 @@
 const oldPassword = document.getElementById("password");
-const newPassword = document.getElementById("newPassword");
+const newPassword = document.getElementById("rePassword");
 const button = document.getElementById("showPass")
 
 button.addEventListener("click", function () {
-    console.log("hola");
-    console.log("Este es el boton "+button);
+    
+    if(button.className.includes("showPass")) {
+        button.classList.add("eye-x");
+        button.classList.remove("showPass")
+      }else {
+          button.classList.remove("eye-x");
+          button.classList.add("showPass")
+      }
     if (oldPassword.type == "password" && newPassword.type == "password") {
         oldPassword.type = "text";
         newPassword.type = "text";
