fd801e24168bbf88371ab9346c72f92cb8cc143b
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-12 19:14:41 -0300

    loading views just added to register page

---
+++ public/js/loading.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const loadedContainer = document.getElementById("loading");
+    const main = document.getElementById("main")
+
+    loadedContainer.classList.add("hidden");
+    main.classList.remove("hidden")
+    
+}
