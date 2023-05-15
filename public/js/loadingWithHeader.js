ac5f47e989d193d6c5f5c2a097e079d43961cb6a
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-14 22:33:03 -0300

    loading views added to loose views

--- public/js/loadingWithHeader.js
+++ public/js/loadingWithHeader.js
@@ -1,11 +1,14 @@
 window.onload = function () {
     const loadedContainer = document.getElementById("loading");
     const main = document.getElementById("main")
+    const header = document.getElementById("header")
 
     setTimeout(() => {
         
     loadedContainer.classList.add("hidden");
     main.classList.remove("hidden")
+    header.classList.remove("hidden")
+
     
     }, 2000);
     // loadedContainer.classList.add("hidden");
