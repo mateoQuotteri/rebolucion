cd7bc9357275f8c1fe7b56994adfa89f5da694da
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-15 19:21:32 -0300

    when loading views appears the footer is hidden now

--- public/js/loadingWithHeader.js
+++ public/js/loadingWithHeader.js
@@ -2,14 +2,15 @@ window.onload = function () {
     const loadedContainer = document.getElementById("loading");
     const main = document.getElementById("main")
     const header = document.getElementById("header")
+    const footer = document.getElementById("footer")
 
     setTimeout(() => {
         
     loadedContainer.classList.add("hidden");
     main.classList.remove("hidden")
     header.classList.remove("hidden")
+    footer.classList.remove("hidden")
 
-    
     }, 2000);
     // loadedContainer.classList.add("hidden");
     // main.classList.remove("hidden")
