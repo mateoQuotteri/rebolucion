5ac1fbd0501513e3552c144f09e8b3a38f46f2ca
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-07 20:05:29 -0300

    teacher views

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,14 @@
+const DB_USER = process.env.DB_USER;
+const DB_USERNAME = process.env.DB_USERNAME;
+
+const DB_PASS = process.env.DB_PASS;
+
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "123456",
-    "database": "rebolucion_db",
+    "username": DB_USERNAME || "root",
+    "password": DB_PASS || "123456",
+    "database":  DB_USER ||"rebolucion_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
