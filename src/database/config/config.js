d5dd29a9b2ceccd7dd865f3f17fe078b6a70eca2
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-09 18:44:05 -0300

    tryng to connect with db V3

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,5 +1,8 @@
-require('dotenv').config(); // Cargar variables de entorno desde .env
-
+const DB_USER = process.env.DB_USER;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASS = process.env.DB_PASS;
+const DB_PORT = process.env.DB_PORT;
+const DB_HOST = process.env.DB_HOST;
 
 module.exports = {
   "development": {
