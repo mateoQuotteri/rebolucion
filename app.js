9bf9d524dd67a41bc404c3e059006c87134591bb
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-10 20:12:42 -0300

    adding dotenv to appjs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const teacherRoutes = require("./src/routes/teacherRoutes");
 const bodyParser = require('body-parser');
 const userAuth = require("./src/middlewares/userAuth");
 const {Sequelize} = require("sequelize");
-
+require('dotenv').config({path: path.join(__dirname, '../','.env')});
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
