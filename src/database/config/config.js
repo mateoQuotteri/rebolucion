fcf9c0d39ff64c43ce93a6a83082f3b189061629
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-10 16:58:04 -0300

    new config js 2

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME || "root",
-    "password": process.env.DB_PASS || "123456",
-    "database": process.env.DB_USER || "rebolucion_db",
-    "host": process.env.DB_HOST || "127.0.0.1",
-    "port": process.env.DB_PORT || "3306", 
+    "username": process.env.MYSQL_USER || "root",
+    "password": process.env.MYSQLPASSWORD || "123456",
+    "database":  process.env.MYSQLDATABASE || "rebolucion_db",
+    "host": process.env.MYSQLHOST || "127.0.0.1",
+    "port": process.env.MYSQLPORT || "3306", 
     "dialect": "mysql"
   },
   "test": {
