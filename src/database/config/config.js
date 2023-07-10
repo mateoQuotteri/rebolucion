e523d1a8002bb755c9722c9f34d11423aba2276b
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-09 22:37:03 -0300

    doing again the config of database v146541518151

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,29 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize({
-  username : process.env.DB_USERNAME,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_DABATABASE,
-  dialect : "mysql",
-  port : process.env.DB_PORT,
-  host : process.env.DB_HOST,
-})
 
 module.exports = sequelize;
+
+module.exports ={
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  
+}
