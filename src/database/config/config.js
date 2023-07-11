8f52309434ad0cf0a986e1e8b7ad5c7f0dee1d46
Mateo Quotteri <quotterimateo@gmail.com>
2023-07-11 20:08:51 -0300

    new config 2

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,6 +16,35 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
+    "username": "root",
+    "password": "HqnfTEl9lsnQ4kUtLcCy",
+    "database": "railway",
+    "host": "containers-us-west-18.railway.app",
+    "port" : "6942", 
+    "dialect": "mysql"
+  },
+}
+
+
+/**
+ *module.exports = {
+  "development": {
+    "username": process.env.MYSQL_USER || "root",
+    "password": process.env.MYSQLPASSWORD || "123456",
+    "database":  process.env.MYSQLDATABASE || "rebolucion_db",
+    "host": process.env.MYSQLHOST || "127.0.0.1",
+    "port": process.env.MYSQLPORT || "3306", 
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQLPASSWORD,
+    "database": process.env.MYSQLDATABASE,
+    "host": process.env.MYSQLHOST,
+    "port" :process.env.MYSQLPORT ,
+    "dialect": "mysql"
+  },
+  "production": {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQLPASSWORD,
     "database": process.env.MYSQLDATABASE,
@@ -25,3 +54,5 @@ module.exports = {
   },
 }
 
+
+ */
