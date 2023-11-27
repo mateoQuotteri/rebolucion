09f89afa3508a0c8a0be52eb28f9b12a4cdaf92a
Mateo Quotteri <quotterimateo@gmail.com>
2023-11-27 17:03:17 -0300

    resources viwes done(?)

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -49,6 +49,40 @@ errorGoogleAuth : (req,res)=>{
     res.render("errorAuth")
 },
 showResources :  (req,res)=>{
-    res.render("resources")
+        const resources= [{
+            name: "Bitcoin",
+            url: "http://localhost:3000/resources/bitcoin",
+            ready : true,
+            icon : "fa-brands fa-bitcoin"
+           },
+           {
+            name: "Ethereum",
+            url: "",
+            ready : false,
+            icon: "fa-brands fa-ethereum"
+           }
+           ,
+           {
+            name: "AI",
+            url: "",
+            ready : false,
+            icon: "fa-solid fa-brain"
+           }
+           ,
+           {
+            name: "Edicion de Video",
+            url: "",
+            ready : false,
+            icon: "fa-solid fa-video"
+           }
+           ,
+           {
+            name: "DeFi",
+            url: "",
+            ready : false,
+            icon: "fa-solid fa-square-poll-vertical"
+           }
+        ]
+        res.render("resources", { resources: resources})
 },
 }
