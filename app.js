21d1403135c0cac314a8b9cf82e5e57796363ffe
Mateo Quotteri <quotterimateo@gmail.com>
2023-11-28 14:31:46 -0300

    resources viwes done

--- app.js
+++ app.js
@@ -1,74 +1,65 @@
 const express = require('express');
 const app = express();
-const methodOverride = require("method-override");
+const methodOverride = require("method-override")
 const mainRoutes = require("./src/routes/mainRoutes");
 const userRoutes = require("./src/routes/userRoutes");
+const resourcesRoutes = require("./src/routes/resourcesRoutes");
+
 const productRoutes = require("./src/routes/productRoutes");
-const unitRoutes = require("./src/routes/unitRoutes");
-const teacherRoutes = require("./src/routes/teacherRoutes");
-const bodyParser = require('body-parser');
-const userAuth = require("./src/middlewares/userAuth");
-const {Sequelize} = require("sequelize");
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '../','.env')});
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const unitRoutes = require("./src/routes/unitRoutes")
+const teacherRoutes = require("./src/routes/teacherRoutes")
+const bodyParser = require('body-parser')
+const userAuth = require("./src/middlewares/userAuth")
+let session = require("express-session")
+const passport = require("passport")
+const PORT =  3000
 
-const passport = require("passport");
-const PORT = process.env.PORT || 3000;
-const sequelize = new Sequelize({
-  database: process.env.MYSQLDATABASE,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQLPASSWORD,
-  host: process.env.MYSQLHOST,
-  port :process.env.MYSQLPORT,
-  dialect: 'mysql'
+app.listen(PORT, () => {
+  console.log("rebolucion is starting");
 });
-
-  // Verificar conexión a la base de datos
-  sequelize.authenticate()
-  .then(() => {
-    console.log('Conexión establecida con la base de datos.');
-  })
-  .catch(err => {
-    console.error('No se pudo conectar a la base de datos:', err);
-  });
-  
-  // Configurar sequelize en tu aplicación
-  app.set('sequelize', sequelize);
-  
-  
-  
-  app.listen(PORT, '0.0.0.0', () => {
-
-    console.log('Servidor iniciado en el puerto', PORT);
-  });
-
 app.set('views', __dirname + '/src/views');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({extended:false}))
+ app.use(express.json())
+
+ 
+ 
+ app.use(
+   session({
+     secret: "mensaje secreto",
+     resave: false,
+     saveUninitialized: false,
+    })
+    )
+    
+    
+     app.use(passport.initialize())
+     // init passport on every route call.app.use(passport.session()) 
+     app.use(passport.session())   
+     // allow passport to use "express-session".
 
 
+     app.use(userAuth)
 
-app.use(session({
-  secret: 'my-secret-key',
-}));
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded())
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(userAuth);
+// parse application/json
+//  app.use(bodyParser.json())
 
 app.use(methodOverride("_method"));
 
 app.use('/', mainRoutes);
-app.use('/user', userRoutes);
-app.use('/module', productRoutes);
-app.use("/unit", unitRoutes);
-app.use("/teacher", teacherRoutes);
+app.use('/user', userRoutes)
+app.use('/module', productRoutes)
+app.use("/unit", unitRoutes)
+app.use("/teacher", teacherRoutes)
+app.use("/resources", resourcesRoutes)
+
 
 app.use((req, res, next) => {
-  res.status(404).render("not-found");
-  next();
-});
+  res.status(404).render("not-found")
+  next()
+})
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,88 +1,86 @@
-const { validationResult } = require("express-validator")
+const { validationResult } = require("express-validator");
 
 module.exports = {
-    index : (req,res)=>{
-
-        const thingsToLearn= [{
-            name: "Herramientas",
-            image: "web3Card.png",
-            icon : "fa-solid fa-gear"
-           },
-           {
-            name: "Blockchain",
-            image: "blockchainCard.png",
-            icon : "fa-solid fa-cubes",
-           },
-           {
-            name: "Habilidades digitales",
-            image: "contratoCard.png",
-            icon :"fa-solid fa-user-tie"
-           },
-           {
-            name: "DeFi",
-            image: "defiCard.png",
-            icon : "fa-solid fa-chart-column",
-           },
-           {
-            name: "Bitcoin",
-            image: "bitcoinCard.png",
-            icon : "fa-brands fa-bitcoin",
-           },
-           {
-            name: "Ethereum",
-            image: "ethereumCard.png",
-            icon : "fa-brands fa-ethereum",
-           },
-        ]
-        res.render("home", { thingsToLearn})
-    },
-    contact : (req,res)=>{
-        res.render("contact")
-},
-aviso : (req,res)=>{
-    res.render("aviso")
-},
-showAdminPanel : (req,res)=>{
-    res.render("adminPanel")
-},
-errorGoogleAuth : (req,res)=>{
-    res.render("errorAuth")
-},
-showResources :  (req,res)=>{
-        const resources= [{
-            name: "Bitcoin",
-            url: "http://localhost:3000/resources/bitcoin",
-            ready : true,
-            icon : "fa-brands fa-bitcoin"
-           },
-           {
-            name: "Ethereum",
-            url: "",
-            ready : false,
-            icon: "fa-brands fa-ethereum"
-           }
-           ,
-           {
-            name: "AI",
-            url: "",
-            ready : false,
-            icon: "fa-solid fa-brain"
-           }
-           ,
-           {
-            name: "Edicion de Video",
-            url: "",
-            ready : false,
-            icon: "fa-solid fa-video"
-           }
-           ,
-           {
-            name: "DeFi",
-            url: "",
-            ready : false,
-            icon: "fa-solid fa-square-poll-vertical"
-           }
-        ]
-        res.render("resources", { resources: resources})
-},
-}
+  index: (req, res) => {
+    const thingsToLearn = [
+      {
+        name: "Herramientas",
+        image: "web3Card.png",
+        icon: "fa-solid fa-gear",
+      },
+      {
+        name: "Blockchain",
+        image: "blockchainCard.png",
+        icon: "fa-solid fa-cubes",
+      },
+      {
+        name: "Habilidades digitales",
+        image: "contratoCard.png",
+        icon: "fa-solid fa-user-tie",
+      },
+      {
+        name: "DeFi",
+        image: "defiCard.png",
+        icon: "fa-solid fa-chart-column",
+      },
+      {
+        name: "Bitcoin",
+        image: "bitcoinCard.png",
+        icon: "fa-brands fa-bitcoin",
+      },
+      {
+        name: "Ethereum",
+        image: "ethereumCard.png",
+        icon: "fa-brands fa-ethereum",
+      },
+    ];
+    res.render("home", { thingsToLearn });
+  },
+  contact: (req, res) => {
+    res.render("contact");
+  },
+  aviso: (req, res) => {
+    res.render("aviso");
+  },
+  showAdminPanel: (req, res) => {
+    res.render("adminPanel");
+  },
+  errorGoogleAuth: (req, res) => {
+    res.render("errorAuth");
+  },
+  showResources: (req, res) => {
+    const resources = [
+      {
+        name: "Bitcoin",
+        url: "https://rebolucion-production.up.railway.app/resources/bitcoin",
+        ready: true,
+        icon: "fa-brands fa-bitcoin",
+      },
+      {
+        name: "Ethereum",
+        url: "",
+        ready: false,
+        icon: "fa-brands fa-ethereum",
+      },
+      {
+        name: "AI",
+        url: "",
+        ready: false,
+        icon: "fa-solid fa-brain",
+      },
+      {
+        name: "Edicion de Video",
+        url: "",
+        ready: false,
+        icon: "fa-solid fa-video",
+      },
+      {
+        name: "DeFi",
+        url: "",
+        ready: false,
+        icon: "fa-solid fa-square-poll-vertical",
+      },
+    ];
+    res.render("resources", { resources: resources });
+  },
+};
